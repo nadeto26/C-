be610e7357fc0bf95e591ada2461383f57da71c6
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-09-16 12:44:16 +0300

    C++

---
+++ 02/c++String/c++String.cpp
@@ -0,0 +1,18 @@
+// c++String.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    string str;
+    string name;
+    bool str; == name;
+}
+
+ 
